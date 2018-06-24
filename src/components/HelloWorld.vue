004a540a40a49f50a45e0bcccabbb86b5badf611
wangyuanBoss <wangyuanboss@126.com>
2018-06-24 19:58:34 +0800

    数据实时变更

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ massage }}</h1>
+
+    <input type="text" v-model="msg">
+
+    <button @click="update()">获取数值</button>
+    <br>
+
+
+    <tab :inputValue="msg"></tab>
+  </div>
+</template>
+
+<script>
+
+import tab from '../components/tab/tab'
+
+var massage='数据实时变更';
+
+export default {
+
+  methods: {
+    update: function(){
+
+      alert("当前输入框中值为\""+this.msg+"\"")
+    }
+  },
+
+  name: 'HelloWorld',
+  components:{
+    tab
+  },
+  data(){
+    return {
+      massage: massage,
+      msg: '请输入'
+    }
+  },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+
+</style>
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div> {{inputValue}}</div>
+
+</template>
+
+<style>
+  h1 {
+    font-weight: normal;
+  }
+
+</style>
+
+<script type="text/ecmascript-6">
+
+    export default {
+      props: {
+        inputValue: String  //定义传值的类型<br>
+        }
+    }
+
+</script>
